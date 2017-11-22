5351a2fbbcc9703e77c3953726dd0229a4fb8f9f
Uchenna Edeh <uchenna.edeh@hpe.com>
2017-11-21 18:21:16 -0600

    4 letter words

--- read_english_dictionary.py
+++ read_english_dictionary.py
@@ -10,8 +10,21 @@ def load_words():
     except Exception as e:
         return str(e)
 
+def load_4_letter_words(my_dict):
+    four_letter_word = []
+    fh = open('four_letter_words.txt', 'w')
+    for idx, val in enumerate(my_dict):
+
+        if len(val) == 4:
+            four_letter_word.append(val)
+            fh.write(str(val)+ '\n')
+    fh.close()        
+    return four_letter_word
+
+
 if __name__ == '__main__':
     english_words = load_words()
     # demo print
-    print(english_words["fate"])
-    print(repr(len(english_words)))
+    print repr(len(load_4_letter_words(english_words)))
+    #print(english_words["fate"])
+    #print(repr(len(english_words)))
